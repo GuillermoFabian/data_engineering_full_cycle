68929f1dfdda0e19ba5276e13e7f93a5bb5d7af8
Guillermo Figueroa <guillermofigueroa@Guillermos-MacBook-Pro.local>
2023-02-03 18:01:15 -0300

    second part

--- second_part/flows/deployment/github_deploy.py
+++ second_part/flows/deployment/github_deploy.py
@@ -8,7 +8,7 @@ github_dep = Deployment.build_from_flow(
      flow=etl_web_to_gcs,
      name="github-example",
      storage=github_block,
-     entrypoint="second_part/flows/deployment/etl_web_to_gcs_Q4.py:etl_web_to_gcs")
+     entrypoint="second_part/flows/deployment/etl_web_to_gcs_nov_2020.py:etl_web_to_gcs")
 
 
 if __name__ == "__main__":
